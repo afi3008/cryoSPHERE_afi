98deabbb991f22564d2bb954543e353f251bcf68
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-10-10 14:26:41 +0700

    fixing quotes inside of f strings

--- model/utils.py
+++ model/utils.py
@@ -237,17 +237,17 @@ def parse_yaml(path):
     logging.info(f"Running cryoSPHERE on folder: {folder_path}")
     logging.info(f"Running cryoSPHERE using device: {device}")
     logging.info(f"Find checkpoints at {path_results}")
-    logging.info(f"Using particles: {particles_path}. Using starfile: {experiment_settings["star_file"]}.")
+    logging.info(f"Using particles: {particles_path}. Using starfile: {experiment_settings['star_file']}.")
     logging.info(f"Running the amortized version of cryoSPHERE: {amortized}. Training for {N_epochs} epochs.")
     logging.info(f"Image size: {Npix}. Pixel size: {apix}. Running cryoSPHERE on downsampled images of size: {Npix_downsize} with pixel size {apix_downsize}.")
-    logging.info(f"""Low pass filtering bandwidth: {bandwidth}. Input images mask radius: {experiment_settings["input_mask_radius"]}.
+    logging.info(f"""Low pass filtering bandwidth: {bandwidth}. Input images mask radius: {experiment_settings['input_mask_radius']}.
         Correlation loss radius: {experiment_settings["loss_mask_radius"]}.""")
-    logging.info(f"Base structure: {experiment_settings["base_structure_path"]} with {N_residues} residues.")
+    logging.info(f"Base structure: {experiment_settings['base_structure_path']} with {N_residues} residues.")
     logging.info(f"Latent dimension: {latent_dim}")
     if amortized:
-        logging.info(f"Encoder hidden layers: {experiment_settings["encoder"]["hidden_dimensions"]}")
+        logging.info(f"Encoder hidden layers: {experiment_settings['encoder']['hidden_dimensions']}")
 
-    logging.info(f"Decoder hidden layers: {experiment_settings["decoder"]["hidden_dimensions"]}")
+    logging.info(f"Decoder hidden layers: {experiment_settings['decoder']['hidden_dimensions']}")
     logging.info(f"Batch size: {batch_size}.")
 
 
