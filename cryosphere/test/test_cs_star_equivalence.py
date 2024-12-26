93d94902c8587039c5d222c8594a854e724640c2
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-12-26 12:06:48 +0100

    creating unit test for dataset

---
+++ cryosphere/test/test_cs_star_equivalence.py
@@ -0,0 +1,69 @@
+path = os.path.abspath("model")
+sys.path.append(path)
+import torch
+import unittest
+from model.dataset import ImageDataSet
+from torch.utils.data import DataLoader
+
+class TestCsStarEquivalence(unittest.TestCase):
+	"""
+	This class takes as input a cryoSparc dataset and the same dataset converted to star (RELION) format. 
+	It tests if the CTF, images and poses are the same
+	"""
+	def setUp(self):
+		star_particles_path = "test_apoferritin/particles/"
+		star_config = {"file": "test_apoferritin/particles/particles.star"}
+		cs_file = {"file": "test_apoferritin/J25_split_0_exported.cs"}
+		self.cs_dataset = ImageDataSet(apix=1.428, side_shape=256, star_cs_file_config=cs_file,particles_path="test_apoferritin")
+		self.star_dataset = ImageDataSet(apix=1.428, side_shape=256, star_cs_file_config=star_config,particles_path="test_apoferritin/particles/")
+
+	def test_images(self):
+		torch.manual_seed(0)
+		data_loader_cs = iter(DataLoader(self.cs_dataset, batch_size=1000, shuffle=True, num_workers = 4, drop_last=True))
+		data_loader_star = iter(DataLoader(self.star_dataset, batch_size=1000, shuffle=True, num_workers = 4, drop_last=True))
+
+		_, batch_images_cs, batch_poses_cs, batch_poses_translation_cs, fproj_cs = next(data_loader_cs)
+		_, batch_images_star, batch_poses_star, batch_poses_translation_star, fproj_star = next(data_loader_star)
+
+		diff = torch.max(torch.abs(batch_images_cs - batch_images_star)).detach().cpu().numpy()
+		self.assertAlmostEqual(diff, 0.0, 5)
+
+	def test_rotations(self):
+		torch.manual_seed(1)
+		data_loader_cs = iter(DataLoader(self.cs_dataset, batch_size=1000, shuffle=True, num_workers = 4, drop_last=True))
+		data_loader_star = iter(DataLoader(self.star_dataset, batch_size=1000, shuffle=True, num_workers = 4, drop_last=True))
+
+		_, batch_images_cs, batch_poses_cs, batch_poses_translation_cs, fproj_cs = next(data_loader_cs)
+		_, batch_images_star, batch_poses_star, batch_poses_translation_star, fproj_star = next(data_loader_star)
+
+		diff = torch.max(torch.abs(batch_poses_cs - batch_poses_star))
+		self.assertAlmostEqual(diff, 0.0, 5)
+
+	def test_translations(self):
+		torch.manual_seed(2)
+		data_loader_cs = iter(DataLoader(self.cs_dataset, batch_size=1000, shuffle=True, num_workers = 4, drop_last=True))
+		data_loader_star = iter(DataLoader(self.star_dataset, batch_size=1000, shuffle=True, num_workers = 4, drop_last=True))
+
+		_, batch_images_cs, batch_poses_cs, batch_poses_translation_cs, fproj_cs = next(data_loader_cs)
+		_, batch_images_star, batch_poses_star, batch_poses_translation_star, fproj_star = next(data_loader_star)
+
+		diff = torch.max(torch.abs(batch_poses_translation_cs - batch_poses_translation_star))
+		self.assertAlmostEqual(diff, 0.0, 5)
+
+	def test_fproj(self):
+		torch.manual_seed(3)
+		data_loader_cs = iter(DataLoader(self.cs_dataset, batch_size=1000, shuffle=True, num_workers = 4, drop_last=True))
+		data_loader_star = iter(DataLoader(self.star_dataset, batch_size=1000, shuffle=True, num_workers = 4, drop_last=True))
+
+		_, batch_images_cs, batch_poses_cs, batch_poses_translation_cs, fproj_cs = next(data_loader_cs)
+		_, batch_images_star, batch_poses_star, batch_poses_translation_star, fproj_star = next(data_loader_star)
+
+		diff = torch.max(torch.abs(fproj_cs - fproj_star)).detach().cpu().numpy()
+		self.assertAlmostEqual(diff, 0.0, 5)
+
+
+
+
+
+
+
