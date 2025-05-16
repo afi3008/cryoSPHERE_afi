8391aa935b9e6e56cb4852b96c75c411b6a62508
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2025-05-16 13:49:28 +0200

    updating the parameter files

--- cryosphere/model/loss.py
+++ cryosphere/model/loss.py
@@ -369,7 +369,7 @@ def compute_loss(predicted_images, images, segmentation_image, latent_mean, late
         structural_loss_parameters["connect_distances"])
 
     if structural_loss_parameters["clash_pairs"] is None:
-        clashing_loss = compute_clashing_distances(predicted_structures, device)
+        clashing_loss = compute_clashing_distances(predicted_structures, device, cutoff=experiment_settings["loss"]["clashing_loss"]["clashing_cutoff"])
     else:
         clashing_loss =  calc_clash_loss(predicted_structures, structural_loss_parameters["clash_pairs"], clash_cutoff=experiment_settings["loss"]["clashing_loss"]["clashing_cutoff"])
 
