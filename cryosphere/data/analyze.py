99e07374dac469bf3498be5771b0ee1086268fab
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2025-06-06 10:40:42 +0200

    again

--- cryosphere/data/analyze.py
+++ cryosphere/data/analyze.py
@@ -168,10 +168,12 @@ def sample_latent_variables(gpu_id, world_size, vae, dataset, batch_size, output
         batch_images = batch_images.flatten(start_dim=-2)
         #batch_latent_mean_list = [torch.zeros_like(batch_images[:, :2], device=batch_images.device) for _ in range(world_size)]
         latent_variables, latent_mean, latent_std = vae.module.sample_latent(batch_images, indexes)
-        all_latent_variables.append(latent_mean.detach().cpu().numpy())
+        #all_latent_variables.append(latent_mean.detach().cpu().numpy())
         batch_latent_mean_list = [torch.zeros_like(latent_mean, device=latent_mean.device) for _ in range(world_size)]
         batch_indexes = [torch.zeros_like(indexes, device=batch_images.device) for _ in range(world_size)]
         if gpu_id == 0:
+            batch_latent_mean_list = [torch.zeros_like(latent_mean, device=latent_mean.device) for _ in range(world_size)]
+            batch_indexes = [torch.zeros_like(indexes, device=batch_images.device) for _ in range(world_size)]
             gather(latent_mean, batch_latent_mean_list)
             gather(indexes, batch_indexes)
         else:
